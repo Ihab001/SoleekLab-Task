61c374aa53eccd63287524752f1bba1f5962aabd
ihab001 <ehab57433@student.must.edu.eg>
2018-10-20 05:15:45 +0200

    adding files

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -14,46 +14,44 @@ import { Input } from '@angular/core';
 })
 export class ProductsTableComponent implements OnInit {
 
-
-  selectedUser: any;
-  
- 
+//getting started with variables that will be used 
+  selectedProduct: any;
   productData: Observable<any[]>;
+
+//intializing services that will be used in our project
   constructor(private prodServ:ProductsService,private  router :Router) { 
-this.selectedUser = 
+   this.selectedProduct = 
 { sku:'0' , name:'0' , image:'0' , category:'0' , price:'0',date:'0' };
 
-  }
+}
 
+//this call the add function from our service
 addProduct(product)
 {
 this.prodServ.addProduct(product);
-
 }
 
-RowSelected(u,x)
+//this to get selected product 
+productSelected(Tableproduct,x)
 {
-  this.selectedUser=u;
+  this.selectedProduct=Tableproduct;
 }
 
+//this function send new data edited of product and it's key to be sent to firebase 
 editProduct(formEdit,key)
 {
 this.prodServ.updateProduct(formEdit,key);
-
-
-
 }
 
+//delete function takes key of product and confirm delete and then deletes
 delete(key)
 { 
   if(confirm("Are you sure to delete this product ? ")) {
     this.prodServ.deleteProduct(key);
   }
     
- 
-
  }
-
+//this is to initialize products table with products once the application is started 
   ngOnInit() {
 
   this.productData = this.prodServ.getProducts().snapshotChanges().pipe(map(changes => {
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -7,24 +7,28 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 })
 export class ProductsService {
   products: AngularFireList<any>;
-  
+  //this service is used to be the layer between the firebase and the componant 
 
   constructor(private firebase:AngularFireDatabase) { }
-
+//this function used to get all the products from firebase
   getProducts(){
    return this.products= this.firebase.list('products');
-   
-
   }
+//this function for adding new product into firebase node called products
+
   addProduct(product){
     return this.firebase.list('products').push(product);
   }
+
+  //this function to update already added product into firebase 
   updateProduct(product,key)
   {
     return this.firebase.list('products').update(key,product);
   }
+//this function takes key as an argument and deletes this product
   deleteProduct(key)
   {
     this.firebase.list('products').remove(key);
   }
+
 }
